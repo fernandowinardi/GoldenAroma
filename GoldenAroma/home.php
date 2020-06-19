e93c4dd29304c2aaf260c30ad9080329d07c4f07
oceanryan <44290450+oceanryan@users.noreply.github.com>
2020-06-19 14:55:47 +0700

    Add files via upload
    navbar working

---
+++ GoldenAroma/home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<head>
+    <title>Golden Aroma</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+    <a href="#"><img class="logo" src="C:\Users\Fernando Winardi\PhpstormProjects\GoldenAroma\GA\frontend\Images\logo.png" alt="(GA logo)"></a>
+    <nav>
+        <ul class="navbar">
+            <li><a class="option" id="active">Welcome</a></li>
+            <li><a class="option">Shop Tea</a></li>
+            <li><a class="option">Shop Coffee</a></li>
+            <li><a class="option">About</a></li>
+            <li><a class="option">Contact us</a></li>
+        </ul>
+    </nav>
+    <a class="login">Sign in/Sign up</a>
+    <a href="#"><img class="basket" src="C:\Users\Fernando Winardi\PhpstormProjects\GoldenAroma\GA\frontend\Images\basket.svg" alt="(basket logo)"></a>
+</header>
+<hr class="line1">
+</body>
+</html>
